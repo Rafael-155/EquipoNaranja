2695fe0b735fbbce33c7df91049b0402ea614112
Rafael-155 <ernesto_vera@usmp.pe>
2024-04-26 14:36:43 -0500

    equiponaranja

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -7,6 +7,6 @@ public class ApplicationDbContext : IdentityDbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
-    {
-    }
+        {}
+        public DbSet<EquipoNaranja.Models.Contacto> DataContacto {get; set; }
 }
