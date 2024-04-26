1cbdc7d15f64356a05687e27a7ea148b6720bcf1
Rafael-155 <ernesto_vera@usmp.pe>
2024-04-26 02:23:44 -0500

    modificaciones

--- obj/Debug/net8.0/EquipoNaranja.AssemblyInfo.cs
+++ obj/Debug/net8.0/EquipoNaranja.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EquipoNaranja")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9f56e26afdc7a8511f1ad9e3b3755cd64492b35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fee6e6ec2d14a5cb2eee7f8b6d7e9efcca62a40d")]
 [assembly: System.Reflection.AssemblyProductAttribute("EquipoNaranja")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EquipoNaranja")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
